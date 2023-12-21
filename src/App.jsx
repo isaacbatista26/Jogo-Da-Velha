9c9313fad2121a8f46e985f4727f627303065f64
Joao <jvcosta1502@gmail.com>
2023-12-21 19:00:24 -0300

    Pagina-Inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Game from './Components/Game'
+import ReactGrannysGame from './Components/ReactGrannysGame';
 
 function App() {
 
   return (
     <div className="App">
-      <Game />
+      {/*<Game />*/}
+      <ReactGrannysGame />
     </div>
   );
 }
