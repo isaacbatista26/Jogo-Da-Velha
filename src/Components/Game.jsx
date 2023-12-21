1c4bab0efe9c7d513ec4f13e0c59483dc5dc67ef
isaacbatista26 <isaacbatista2266@gmail.com>
2023-12-21 13:13:18 -0300

    changes

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function getInitialState() {
+    const state = {};
+    for(let r = 0; r < 3; r++) {
+        for(let c = 0; c < 3; c++ ){
+            state[`${r}-${c}`] = null;
+        }
+    }
+    return state
+}
 
 const Game = () => {
+    const [values, setValues] = useState(getInitialState);
+
     return (
         <div className="Game">
             <div className="Game_board">
---
+++ src/Components/ReactGrannysGame.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const ReactGrannysGame = () => {
+
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default ReactGrannysGame;
